5b917b57eb9d517c465109ba1c915031b4bb2581
Richard C Isaacson <richard@eucalyptus.com>
2013-08-30 02:56:47 -0500

    Progress commit.

--- src/main/java/com/eucalyptus/wormhole/validation/CompanyToBucketValidator.java
+++ src/main/java/com/eucalyptus/wormhole/validation/CompanyToBucketValidator.java
@@ -2,21 +2,26 @@ package com.eucalyptus.wormhole.validation;
 
 import com.eucalyptus.wormhole.model.CompanyToBucket;
 
+import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 /**
  */
-public class CompanyToBucketValidator implements Validator{
+@Component
+public class CompanyToBucketValidator implements Validator {
+
   @Override
   public boolean supports(Class<?> aClass) {
     return CompanyToBucket.class.isAssignableFrom(aClass);
   }
+
   @Override
   public void validate(Object target, Errors errors) {
     CompanyToBucket companytoBucket = (CompanyToBucket) target;
     ValidationUtils.rejectIfEmpty(errors, "company", "companyToBucket.company.empty");
     ValidationUtils.rejectIfEmpty(errors, "bucket", "companyToBucket.bucket.empty");
   }
+
 }
