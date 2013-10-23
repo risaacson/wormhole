c7d388e47dd336bc3d73af74e5163cd6788d4258
Richard C Isaacson <richard.c.isaacson@gmail.com>
2013-10-22 22:30:50 -0500

    We are now serving images. Well and some other fixes.

--- src/main/java/com/eucalyptus/wormhole/init/WebAppConfig.java
+++ src/main/java/com/eucalyptus/wormhole/init/WebAppConfig.java
@@ -10,10 +10,7 @@ import com.eucalyptus.wormhole.model.BlackholeProperties;
 import org.hibernate.ejb.HibernatePersistence;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.*;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -21,19 +18,21 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 @Configuration
+//@Import({ WebAppMappingConfig.class })
 @EnableWebMvc
 @EnableTransactionManagement
 @ComponentScan("com.eucalyptus.wormhole")
 @PropertySource("classpath:wormhole.properties")
 @EnableJpaRepositories("com.eucalyptus.wormhole.repository")
-public class WebAppConfig extends WebMvcConfigurationSupport {
+public class WebAppConfig extends WebMvcConfigurerAdapter {
 
   final Logger logger = LoggerFactory.getLogger(WebAppConfig.class);
 
@@ -59,6 +58,7 @@ public class WebAppConfig extends WebMvcConfigurationSupport {
 
   @Bean
   public DataSource dataSource() {
+    logger.debug("ENTER: Bean");
     DriverManagerDataSource dataSource = new DriverManagerDataSource();
     dataSource.setDriverClassName(env.getRequiredProperty(PROPERTY_NAME_DATABASE_DRIVER));
     dataSource.setUrl(env.getRequiredProperty(PROPERTY_NAME_DATABASE_URL));
@@ -68,7 +68,8 @@ public class WebAppConfig extends WebMvcConfigurationSupport {
   }
 
   @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+  public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+    logger.debug("ENTER: Bean");
     LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
     entityManagerFactoryBean.setDataSource(dataSource());
     entityManagerFactoryBean.setPersistenceProviderClass(HibernatePersistence.class);
@@ -78,6 +79,7 @@ public class WebAppConfig extends WebMvcConfigurationSupport {
   }
 
   private Properties hibProperties() {
+    logger.debug("ENTER: Method");
     Properties properties = new Properties();
     properties.put(PROPERTY_NAME_HIBERNATE_DIALECT,	env.getRequiredProperty(PROPERTY_NAME_HIBERNATE_DIALECT));
     properties.put(PROPERTY_NAME_HIBERNATE_SHOW_SQL, env.getRequiredProperty(PROPERTY_NAME_HIBERNATE_SHOW_SQL));
@@ -86,13 +88,30 @@ public class WebAppConfig extends WebMvcConfigurationSupport {
 
   @Bean
   public JpaTransactionManager transactionManager() {
+    logger.debug("ENTER: Bean");
     JpaTransactionManager transactionManager = new JpaTransactionManager();
     transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
     return transactionManager;
   }
 
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    logger.debug("ENTER: Method");
+    registry.addResourceHandler("/resources/images/**").addResourceLocations("/resources/images/").setCachePeriod(31556926);
+//    registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/META-INF/resources/webjars/").setCachePeriod(31556926);
+//    registry.addResourceHandler("/css/**").addResourceLocations("/css/").setCachePeriod(31556926);
+//    registry.addResourceHandler("/img/**").addResourceLocations("/img/").setCachePeriod(31556926);
+//    registry.addResourceHandler("/js/**").addResourceLocations("/js/").setCachePeriod(31556926);
+  }
+
+  @Override
+  public void configureDefaultServletHandling(DefaultServletHandlerConfigurer defaultServletHandlerConfigurer) {
+    defaultServletHandlerConfigurer.enable();
+  }
+
   @Bean
   public UrlBasedViewResolver setupViewResolver() {
+    logger.debug("ENTER: Bean");
     UrlBasedViewResolver resolver = new UrlBasedViewResolver();
     resolver.setPrefix("/WEB-INF/pages/");
     resolver.setSuffix(".jsp");
@@ -102,6 +121,7 @@ public class WebAppConfig extends WebMvcConfigurationSupport {
 	
   @Bean
   public ResourceBundleMessageSource messageSource() {
+    logger.debug("ENTER: Bean");
     ResourceBundleMessageSource source = new ResourceBundleMessageSource();
     source.setBasename(env.getRequiredProperty("message.source.basename"));
     source.setUseCodeAsDefaultMessage(true);
@@ -109,16 +129,8 @@ public class WebAppConfig extends WebMvcConfigurationSupport {
   }
 
   @Bean
-  @Override
-  public RequestMappingHandlerMapping requestMappingHandlerMapping() {
-    RequestMappingHandlerMapping handlerMapping = super.requestMappingHandlerMapping();
-    handlerMapping.setUseSuffixPatternMatch(false);
-//    handlerMapping.setUseTrailingSlashMatch(false);
-    return handlerMapping;
-  }
-
-  @Bean
   public AwsProperties awsProperties() {
+    logger.debug("ENTER: Bean");
     AwsProperties properties = new AwsProperties();
     logger.debug( PROPERTY_NAME_AWS_REGION  + " " + env.getRequiredProperty(PROPERTY_NAME_AWS_REGION) );
     properties.setRegion(env.getRequiredProperty(PROPERTY_NAME_AWS_REGION));
@@ -135,6 +147,7 @@ public class WebAppConfig extends WebMvcConfigurationSupport {
 
   @Bean
   public BlackholeProperties blackholeProperties() {
+    logger.debug("ENTER: Bean");
     BlackholeProperties properties = new BlackholeProperties();
     logger.debug( PROPERTY_NAME_BLACKHOLE_PREFIX  + " " + env.getRequiredProperty(PROPERTY_NAME_BLACKHOLE_PREFIX) );
     properties.setPrefix(env.getRequiredProperty(PROPERTY_NAME_BLACKHOLE_PREFIX));
